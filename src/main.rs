bf118a897f11bb89e3d72ba4e09fa3edae3d7af3
mixup <jovalu2015@gmail.com>
2026-05-30 20:32:10 -0700

    Featiure;grep finalizado

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use std::{env, fs, process, error::Error};
-use minigrep::search;
+use minigrep::{search, search_case_insensitive};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::new(&args).unwrap_or_else(|err|{
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1)
     });
 
     if let Err(e) = run(config){
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
@@ -17,15 +17,22 @@ fn main() {
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
 
-    for line in search(&config.query, &contents){
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    }else {
+        search(&config.query, &contents)
+    };
+
+    for line in results{
         println!("{line}");
     }
     Ok(())
 }
 
 struct Config {
-    query: String,
-    file_path: String,
+    pub query: String,
+    pub file_path: String,
+    pub ignore_case: bool,
 }
 
 impl Config{
@@ -33,9 +40,12 @@ impl Config{
         if args.len() < 3 {
             return Err("Not enough arguments");
         }
+
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
         let query = args[1].clone();
         let file_path = args[2].clone();
-        Ok(Config { query, file_path })
+        Ok(Config { query, file_path, ignore_case })
     }
 }
 
